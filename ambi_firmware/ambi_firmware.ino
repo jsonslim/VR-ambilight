eba3866dfc7ae9eb29e2873b19815c7bdbe26dad
spacewormsera <deluxe87d@gmail.com>
2023-06-29 14:24:57 +0200

    arduino update

--- ambi_firmware/ambi_firmware.ino
+++ ambi_firmware/ambi_firmware.ino
@@ -1,17 +1,93 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
+
+const int buttonPin = 2;
+int packetSize = 12; // r+g+b * 4(leds) 
+
+// Variable to store the button state
+volatile int currentMode = 0; // 0 - standby, 1- single pixel mode, 2 - display area calculated
+volatile int previousMode = 0; 
+
+// Variables for debounce logic
+const unsigned long debounceDelay = 50;
+volatile unsigned long lastDebounceTime = 0;
+volatile int lastButtonState = HIGH;
+
+// Network variables
 const char* ssid = "your_SSID";         // WiFi network SSID
 const char* password = "your_PASSWORD"; // WiFi network password
-
-int packetSize = 12; // r+g+b * 4(leds) 
 
 WiFiUDP udp;
 const uint16_t udpPort = 1234; // UDP port to listen on
 IPAddress udpAddress(192, 168, 1, 100); // IP address to listen to
 uint8_t buffer[packetSize];
 
+void IRAM_ATTR buttonISR() {
+  // Check the debounce state of the button
+  if ((millis() - lastDebounceTime) >= debounceDelay) {
+    // Read the current button state
+    int buttonState = digitalRead(buttonPin);
+
+    // Check if the button state has changed
+    if (buttonState != lastButtonState) {
+      // Update the last button state
+      lastButtonState = buttonState;
+
+      if (buttonState == LOW) {
+        mode++;
+        if (mode > 2) {
+          mode = 0;
+        }
+
+        // Print the updated state to the serial monitor
+        Serial.print("State changed to: ");
+        Serial.println(mode);
+      }
+    }
+  }
+
+  // Update the debounce time
+  lastDebounceTime = millis();
+}
+
+void updateLeds(){
+  // Check if there's a UDP packet available
+  packetSize = udp.parsePacket();
+
+  if (packetSize) {
+    if (udp.remoteIP() == udpAddress) {
+      // Read the UDP packet into the buffer
+      udp.read(buffer, packetSize);
+
+      // Print the received uint array
+      Serial.print("Received uint array: ");
+      for (int i = 0; i < packetSize; i++) {
+        Serial.print(buffer[i]);
+        Serial.print(" ");
+      }      
+      Serial.println();
+
+      // todo send to Leds
+    } else {
+      // Ignore packets from other IP addresses
+      udp.flush();
+    }
+  }
+}
+
+void ledsBlink(bool color){
+// 0 - green, 1 - orange or smthn
+}
+
+void sendUdpMsg(String msg){
+
+}
+
 void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonISR, FALLING); // maybe RISING is better
+
   Serial.begin(115200);
 
   // Connect to WiFi network
@@ -33,30 +109,27 @@ void setup() {
 }
 
 void loop() {
-  // Check if there's a UDP packet available
-  packetSize = udp.parsePacket();
-  if (packetSize) {
-    if (udp.remoteIP() == udpAddress) {
-      // Read the UDP packet into the buffer
-      udp.read(buffer, packetSize);
 
-      // Print the received uint array
-      Serial.print("Received uint array: ");
-      for (int i = 0; i < packetSize; i++) {
-        Serial.print(buffer[i]);
-        Serial.print(" ");
-      }      
-      Serial.println();
-
-      updateLeds();
-    } else {
-      // Ignore packets from other IP addresses
-      udp.flush();
+  if(currentMode == 0){
+    if(currentMode != previousMode){
+      sendUdpMsg("standby");
+      // todo leds off
+      previousMode = currentMode;
     }
-  }
-}
+  } else if (mode == 1) {
+    if(previousMode != currentMode){
+      sendUdpMsg("singlePixel");
+      ledsBlink();
+      previousMode = currentMode;
+    }
+    updateLeds();
 
-updateLeds(){
-    // todo update actual leds
-    Serial.print("leds updated");
+  } else if (mode == 2){
+    if(previousMode != currentMode){
+      sendUdpMsg("dispArea");
+      ledsBlink();
+      previousMode = currentMode;
+    }
+    updateLeds();
+  }  
 }
