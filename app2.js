2962870bbf2fc93a921a69109ad9b0f96a058d1b
Sergey Tarasov <Sergey.Tarasov@zwickroell.com>
2024-03-13 16:27:30 +0100

    polishing

--- app2.js
+++ app2.js
@@ -5,15 +5,14 @@ const fs = require('fs');
 const server = udp.createSocket('udp4');
 
 let l1, l2, l3, l4 = Buffer.alloc(3);
-// const buf = Buffer.allocUnsafe(3);
 
-let captureMode = "dispArea"; // standby / dispArea
+let captureMode = "dispArea"; // standby|dispArea
 
 // default config used if no config.json exists
 let config = {
 	boardIp: "192.168.0.30",      // ESP32 IP
 	boardPort: 5555,              // ESP32 port
-	srvPort: 5554,                // this server port
+	srvPort: 5554,                // this app port
 	screenWidth: 1920,
 	screenHeight: 1080,
 	refreshRate: 24,
@@ -29,7 +28,6 @@ let config = {
 
 function init() {
 	try {
-		// read config.json and set variables in config var
 		const fileData = fs.readFileSync('./config.json', 'utf-8');
 		config = JSON.parse(fileData);
 		// console.log(`-=Init successful=- \nip: ${config.ip} \nport: ${config.port}`);
@@ -43,7 +41,7 @@ function init() {
 			console.log('Server ip:' + ipaddr);
 		});
 
-		//emits after the socket is closed using socket.close();
+		//emits after the socket is closed using socket.close(); ???
 		server.on('close', function () {
 			console.log('Socket is closed !');
 		});
@@ -57,16 +55,13 @@ function init() {
 	}
 }
 
+// accepts only 6 bytes format, like '27ae60'
 function hexToRgb(hex) {
 	var r = parseInt(hex.slice(0, 2), 16);
 	var g = parseInt(hex.slice(2, 4), 16);
 	var b = parseInt(hex.slice(4, 6), 16);
 	return [r, g, b];
 }
-
-// hexToRGB('#27ae60ff'); // 'rgba(39, 174, 96, 255)'
-// hexToRGB('27ae60'); // 'rgb(39, 174, 96)'
-// hexToRGB('#fff'); // 'rgb(255, 255, 255)'
 
 // returns average color from selected area
 function captureScreenArea(x, y, width, height) {
@@ -80,6 +75,7 @@ function captureScreenArea(x, y, width, height) {
 	for (let x = 0; x < img.width; x++) {
 		for (let y = 0; y < img.height; y++) {
 			let hex = img.colorAt(x, y);
+			console.log('hex data: ', hex);
 			let rgb = hexToRgb(hex);
 			sumR += rgb[0];
 			sumG += rgb[1];
@@ -107,7 +103,7 @@ function captureScreen(width, height) {
 	l2 = captureScreenArea(0, 0, width, height);        // led2 - top left
 	l3 = captureScreenArea(1919, 0, width, height);     // led3 - top right
 	l4 = captureScreenArea(1919, 540, width, height);   // led4 - bottom right
-	console.log(`l1:`, l1, `l2:`, l2, `l3:`, l3, `l4:`, l4);
+	// console.log(`l1:`, l1, `l2:`, l2, `l3:`, l3, `l4:`, l4);
 }
 
 // emits when any error occurs
